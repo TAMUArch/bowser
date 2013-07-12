22d87f8ccac624baf7e5e7d9fb550ecd537c9702
cjohnson <cjohnson@coa-dhcp-008.arch.tamu.edu>
2013-07-12 14:16:25 -0500

    commit 11.0.2 bowser more verbose

--- bowser.sh
+++ bowser.sh
@@ -8,9 +8,14 @@ pacman -S xorg-twm xorg-xclock xterm
 
 pacman -S openbox chromium openssh rsync
 
-useradd -m guest
+pacman -S flashplayer
 
-echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> /home/guest/.bash_profile
+echo pacman operations complete
+sleep 2s
+
+useradd -m guest
+echo guest user added...
+sleep 2s
 
 mkdir /etc/systemd/system/getty@tty1.service.d/
 touch /etc/systemd/system/getty@tty1.service.d/autologin.conf
@@ -19,8 +24,14 @@ echo '[Service]
 ExecStart=
 ExecStart=-/usr/bin/agetty --autologin guest --noclear %I 38400 linux
 Type=simple' > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo autologin.conf configured...
+sleep 2s
 
 systemctl enable graphical.target
+
+echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' >> /home/guest/.bash_profile
+echo bash profile configured...
+sleep 2s
 
 cp -r /home/guest /opt/
 cd /opt/guest/
@@ -29,6 +40,8 @@ chmod -R a+r .
 cp /home/bowser/.xinitrc /opt/guest/
 chmod a+x .xinitrc
 cp .xinitrc /home/guest/
+echo xinitrc configured...
+sleep 2s
 
 rm /etc/xdg/openbox/menu.xml
 touch /etc/xdg/openbox/menu.xml
@@ -37,8 +50,14 @@ echo '<openbox_menu>
   <separator label="Operation Not Supported" />
 </menu>
 </openbox_menu>' >> /etc/xdg/openbox/menu.xml
+echo openbox root-menu configured...
+sleep 2s
 
 rm /etc/xdg/openbox/rc.xml
 cp /home/bowser/rc.xml /etc/xdg/openbox/rc.xml
+echo chromium maximized...
+sleep 2s
 
+echo rebooting...
+sleep 2s
 reboot
